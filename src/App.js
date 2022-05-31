c7b357ab41754ce0ff4d0c51ae51a8cc6f799ca1
Vivek Verma <mr.vivek2090@email.com>
2022-05-31 18:29:59 +0530

    imdb api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import Search from "./Search";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
+
+  useEffect(() => {
+    axios("https://imdb-api.com/en/API/SearchMovie/k_g77w9fwz/")
+      .then((response) => {
+        console.log(response.data);
+        setAllData(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting fake data: " + error);
+      });
+  }, []);
+  const handleSearch = (event) => {};
+
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,41 @@
+// import {
+//   TextField,
+//   ThemeProvider,
+//   createTheme,
+//   Button,
+// } from "@material-ui/core";
+// import "./Search.css";
+// import SearchIcon from "@material-ui/icons/Search";
+// // import { useState } from "react";
+
+// const Search = () => {
+//   // const [type, setType] = useState(0);
+
+//   const darkTheme = createTheme({
+//     palette: {
+//       type: "dark",
+//       primary: {
+//         main: "#fff",
+//       },
+//     },
+//   });
+//   return (
+//     <div>
+//       <ThemeProvider theme={darkTheme}>
+//         <div style={{ display: "flex", margin: "15px 0" }}>
+//           <TextField
+//             style={{ flex: 1 }}
+//             className="Search movies"
+//             label="Search"
+//             varient="field"
+//           />
+//           <Button varient="contained" style={{ marginLeft: 10 }}>
+//             <SearchIcon />
+//           </Button>
+//         </div>
+//       </ThemeProvider>
+//     </div>
+//   );
+// };
+
+// export default Search;
